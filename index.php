59cf08e12f4f88ec5e0c39302cbbe58b90efc2da
pbmacintyre <pbmacintyre@gmail.com>
2025-08-13 16:17:39 -0300

    First full draft of working code

--- index.php
+++ index.php
@@ -29,28 +29,98 @@ function show_form($message, $print_again = false) {
                 </td>
             </tr>
             <tr class="CustomTable">
-                <td class="CustomTableFullCol">
-                    <a href="list_opt_ins_outs.php"> List Opt Ins / Outs </a>
+                <td class="left_col">
+                    <p style='display: inline;' >Receiving Mobile #:</p>
                 </td>
-                <td class="CustomTableFullCol">
-                    <a href="manage_opts.php"> Manage Opt In / Opt Out list </a>
-                </td>
-                <td class="CustomTableFullCol">
-                    <a href="export_opts.php"> Export Opt In / Opt Out Data </a>
+                <td class="right_col">
+                    <input type="text" name="to_sms_number" >
                 </td>
             </tr>
+            <tr class="CustomTable">
+                <td class="left_col">
+                    <p style='display: inline;' >SMS message body:</p>
+                </td>
+                <td class="right_col">
+                    <textarea name="sms_message_body" ></textarea>
+                </td>
+            </tr>
+            <tr class="CustomTable">
+                <td class="left_col">
+                    <p style='display: inline;' >Select file to attach to SMS Message:</p>
+                </td>
+                <td class="right_col">
+                    <input type="file" name="file_to_mms" id="file_to_mms">
+                </td>
+            </tr>
+            <tr class="CustomTable">
+                <td colspan="2" class="CustomTableFullCol">
+                    <br/>
+                    <input type="submit" class="submit_button" value="   Send MMS   " name="send_mms">
+                </td>
+            </tr>
+            <tr class="CustomTable">
+                <td colspan="2" class="CustomTableFullCol"><hr></td>
+            </tr>
+
         </table>
     </form>
 	<?php
 }
+function check_form () {
+    show_errors();
 
+    $print_again = false;
+    $message = "";
+
+    /* ============================================ */
+    /* ====== START data integrity checks ========= */
+    /* ============================================ */
+
+    $to_sms_number = strip_tags($_POST['to_sms_number']);
+    $sms_message_body = strip_tags($_POST['sms_message_body']);
+    $target_file = basename($_FILES["file_to_mms"]["name"]);
+
+    if ($target_file == "") {
+        $print_again = true;
+        $message = "No file selected to be uploaded";
+    }
+    if ($sms_message_body == "") {
+        $print_again = true;
+        $message = "No sms message body has been provided";
+    }
+    if ($to_sms_number == "") {
+        $print_again = true;
+        $message = "No receiving SMS mobile Number has been provided";
+    }
+
+    /* ========================================== */
+    /* ====== END data integrity checks ========= */
+    /* ========================================== */
+
+    $file_with_path = upload_file($target_file);
+
+    if ($file_with_path == 0) {
+        // a file uploading error occured
+        $print_again = true;
+        $message = $_SESSION['message'];
+    } else {
+        $mms_sent_id = send_mms($to_sms_number, $sms_message_body, $file_with_path ) ;
+        if ($mms_sent_id > 0) {
+            $print_again = true;
+            $message = "SMS Multipart message (MMS) sent successfully (Send id): " . $mms_sent_id ;
+            // clean out the file
+            unlink($file_with_path) ;
+        }
+    }
+    show_form($message, $print_again);
+}
 /* ============= */
 /*  --- MAIN --- */
 /* ============= */
-if (isset($_POST['add_reminder'])) {
+if (isset($_POST['send_mms'])) {
 	check_form();
 } else {
-	$message = "Please your desired action from the links below.";
+	$message = "Please provide the required information.";
 	show_form($message);
 }
 
